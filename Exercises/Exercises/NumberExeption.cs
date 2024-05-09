0135760a40667a4a7978b518426fd160c76ab26f
Angel Cachi <angel.cachi@jalasoft.com>
2024-05-09 08:28:19 -0400

    exercise one

---
+++ Exercises/Exercises/NumberExeption.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public class NumberExeption : Exception
+    {
+        public NumberExeption() : base() { }
+        public NumberExeption(string message) : base(message) 
+        {
+            throw new NumberExeption("The value is not a number");
+        }
+        public NumberExeption(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
---
+++ Exercises/Exercises/One.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    internal class One
+    {
+        public static void MyIntStack()
+        {
+            int[] contents = new int[100];
+
+        }
+        
+        public static int[] AddValues(int[] contents)
+        {
+            List<int> list = contents.ToList();
+            
+            
+
+            try
+            {
+                ReadValue();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            {
+
+            }
+            
+
+        }
+
+        public static int ReadValue()
+        {
+            Console.WriteLine("Please enter a number");
+            string valueInserted = Console.ReadLine();
+            int value = Convert.ToInt32(valueInserted);
+            return value;
+        }
+        
+    }
+}
